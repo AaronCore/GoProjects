631c1305500bdbf16d6d3aab955abe288be09a48
AaronCore <969340992@qq.com>
2021-01-15 16:39:27 +0800

    add 10_exersice_sample.go

--- src/github.com/AaronCore/StudyGo/01_Base/9_func_sample.go
+++ src/github.com/AaronCore/StudyGo/01_Base/9_func_sample.go
@@ -3,10 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("----- 1.函数测试 -----")
 	a := f4(1, 2)
 	fmt.Println(a)
 	a1, a2 := f5()
 	fmt.Println(a1, a2)
+
+	// Go语言中函数传递的都是值（复制、粘贴）
+	fmt.Println("----- 2.函数传递测试 -----")
+	x := [3]int{1, 2, 3}
+	fmt.Println("x：", x)
+	f7(x)
+	fmt.Println("x：", x)
+	y := x
+	y[0] = 55
+	fmt.Println("y：", y)
+
 }
 
 // 无参无返回值函数
@@ -39,4 +51,8 @@ func f5() (int, string) {
 // 同类型参数简写函数
 func f6(a, b string) string {
 	return a + b
+}
+
+func f7(x [3]int) {
+	x[0] = 100
 }
