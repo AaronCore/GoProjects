e4c57115e3cf4f1a9f935acde40f4944f5ec7249
AaronCore <969340992@qq.com>
2021-11-25 16:38:34 +0800

    commit

--- src/go-web/main.go
+++ src/go-web/main.go
@@ -19,8 +19,6 @@ func main() {
 		fmt.Printf("load config from file failed, err:%v\n", err)
 		return
 	}
-	// 创建数据库
-	// sql: CREATE DATABASE bubble;
 	// 连接数据库
 	err := dao.InitMySQL(setting.Conf.MySQLConfig)
 	if err != nil {
