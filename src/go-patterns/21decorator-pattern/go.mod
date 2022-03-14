3add88d7c25a9a698b48913c1124c9116d43036a
AaronCore <969340992@qq.com>
2022-03-14 09:14:40 +0800

    commit

---
+++ src/go-patterns/21decorator-pattern/go.mod
@@ -0,0 +1,3 @@
+module go-patterns/21decorator-pattern
+
+go 1.16
---
+++ src/go-patterns/21decorator-pattern/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// 装饰模式
+func main() {
+	veggiePizza := &veggieMania{}
+
+	//Add cheese topping
+	veggiePizzaWithCheese := &cheeseTopping{
+		pizza: veggiePizza,
+	}
+
+	//Add tomato topping
+	veggiePizzaWithCheeseAndTomato := &tomatoTopping{
+		pizza: veggiePizzaWithCheese,
+	}
+
+	fmt.Printf("Price of veggieMania pizza with tomato and cheese topping is %d\n", veggiePizzaWithCheeseAndTomato.getPrice())
+
+	peppyTofuPizza := &peppyTofu{}
+
+	//Add cheese topping
+	peppyTofuPizzaWithCheese := &cheeseTopping{
+		pizza: peppyTofuPizza,
+	}
+
+	fmt.Printf("Price of peppyTofu with tomato and cheese topping is %d\n", peppyTofuPizzaWithCheese.getPrice())
+}
