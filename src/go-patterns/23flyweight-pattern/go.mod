72eb9af84106a201a2d145ce94760a3f6894bbc9
aaroncore <969340992@qq.com>
2022-03-30 09:54:53 +0800

    commit flyweight-pattern

---
+++ src/go-patterns/23flyweight-pattern/go.mod
@@ -0,0 +1,3 @@
+module go-patterns/23flyweight-pattern
+
+go 1.17
---
+++ src/go-patterns/23flyweight-pattern/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// 享元模式
+func main() {
+	game := newGame()
+	//Add Terrorist
+	game.addTerrorist(TerroristDressType)
+	game.addTerrorist(TerroristDressType)
+	game.addTerrorist(TerroristDressType)
+	game.addTerrorist(TerroristDressType)
+	//Add CounterTerrorist
+	game.addCounterTerrorist(CounterTerroristDressType)
+	game.addCounterTerrorist(CounterTerroristDressType)
+	game.addCounterTerrorist(CounterTerroristDressType)
+
+	dressFactoryInstance := getDressFactorySingleInstance()
+	for dressType, dress := range dressFactoryInstance.dressMap {
+		fmt.Printf("DressColorType: %s\nDressColor: %s\n", dressType, dress.getColor())
+	}
+}
