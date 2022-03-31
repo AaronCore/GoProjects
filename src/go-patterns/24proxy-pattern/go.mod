7b197d6b0bd0c45cb8d308aedfee3907d70b0fd9
aaroncore <969340992@qq.com>
2022-03-31 09:27:30 +0800

    commit proxy-pattern

---
+++ src/go-patterns/24proxy-pattern/go.mod
@@ -0,0 +1,3 @@
+module go-patterns/24proxy-pattern
+
+go 1.17
---
+++ src/go-patterns/24proxy-pattern/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// 代理模式
+func main() {
+	nginxServer := newNginxServer()
+	appStatusURL := "/app/status"
+	createUserURL := "/create/user"
+	httpCode, body := nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+	httpCode, body = nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+	httpCode, body = nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+	httpCode, body = nginxServer.handleRequest(createUserURL, "POST")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+	httpCode, body = nginxServer.handleRequest(createUserURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+}
